238f18918b406f9b1105bfeaaecc16ebf26247bf
Argyroudis Athanasios <argyroudisth@gmail.com>
2024-12-09 09:36:42 +0200

    day1 format fixes

--- src/day1/index.ts
+++ src/day1/index.ts
@@ -1,4 +1,4 @@
-import readInput, { numberComparator } from "../utils"
+import readInput, { numberComparator } from "../utils";
 import { EOL } from "node:os";
 const firstColumn: number[] = [];
 const secondColumn: number[] = [];
@@ -6,40 +6,40 @@ const secondColumn: number[] = [];
 const data = readInput(import.meta.dirname);
 const lines = data.split(EOL);
 
-lines.forEach(line => {
-    const [first, second] = line.trim().split(/\s+/).map(Number);
-    firstColumn.push(first);
-    secondColumn.push(second);
+lines.forEach((line) => {
+  const [first, second] = line.trim().split(/\s+/).map(Number);
+  firstColumn.push(first);
+  secondColumn.push(second);
 });
 
 const firstColumnSorted = firstColumn.sort(numberComparator);
 const secondColumnSorted = secondColumn.sort(numberComparator);
 
 if (firstColumn.length !== secondColumn.length) {
-    throw new Error('wrong length kek');
+  throw new Error("wrong length kek");
 }
 
 let sum = 0;
 for (let i = 0; i < firstColumn.length; i++) {
-    sum += Math.abs(firstColumnSorted[i] - secondColumnSorted[i]);
+  sum += Math.abs(firstColumnSorted[i] - secondColumnSorted[i]);
 }
 
-console.log('Distance: ' + sum.toString());
+console.log("Distance: " + sum.toString());
 
 const occurencies: Record<number, number> = {};
-secondColumn.forEach(number => {
-    if (Object.prototype.hasOwnProperty.call(occurencies, number)) {
-        occurencies[number]++;
-    } else {
-        occurencies[number] = 1;
-    }
+secondColumn.forEach((number) => {
+  if (Object.prototype.hasOwnProperty.call(occurencies, number)) {
+    occurencies[number]++;
+  } else {
+    occurencies[number] = 1;
+  }
 });
 
 let similarityScore = 0;
-firstColumn.forEach(number => {
-    if (Object.prototype.hasOwnProperty.call(occurencies, number)) {
-        similarityScore += number * occurencies[number];
-    }
+firstColumn.forEach((number) => {
+  if (Object.prototype.hasOwnProperty.call(occurencies, number)) {
+    similarityScore += number * occurencies[number];
+  }
 });
 
 console.log("Similarity score: " + similarityScore.toString());
